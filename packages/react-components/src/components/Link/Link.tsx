41edc705211c3ba9f117326b5c50143417eed8b5
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-11-05 16:01:26 -0800

    basic implementation of Link component

---
+++ packages/react-components/src/components/Link/Link.tsx
@@ -0,0 +1,31 @@
+import {
+  Link as ReactAriaLink,
+  LinkProps as ReactAriaLinkProps,
+} from "react-aria-components";
+
+import "./Link.css";
+import React from "react";
+
+export interface LinkProps extends ReactAriaLinkProps {
+  iconLeft?: React.ReactNode;
+  iconRight?: React.ReactNode;
+}
+
+export default function Link({
+  children,
+  iconLeft,
+  iconRight,
+  ...props
+}: LinkProps) {
+  return (
+    <ReactAriaLink className="bcds-react-aria-Link" {...props}>
+      {iconLeft && (
+        <span className="bcds-react-aria-Link--Icon">{iconLeft}</span>
+      )}
+      <>{children}</>
+      {iconRight && (
+        <span className="bcds-react-aria-Link--Icon">{iconRight}</span>
+      )}
+    </ReactAriaLink>
+  );
+}
