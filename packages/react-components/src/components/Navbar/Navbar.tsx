7aacbefb3e81192b58315f38ea076a2dc78998b9
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-11-07 15:14:17 -0800

    add separators between navbar items

--- packages/react-components/src/components/Navbar/Navbar.tsx
+++ packages/react-components/src/components/Navbar/Navbar.tsx
@@ -1,14 +1,24 @@
 import "./Navbar.css";
+import React from "react";
 
 export interface NavbarProps extends React.PropsWithChildren {
   children?: React.ReactNode;
 }
 
 export default function Navbar({ children, ...props }: NavbarProps) {
+  const childrenArray = React.Children.toArray(children);
+  const Separator = () => (
+    <span className="bcds-Navbar--Separator" role="separator" />
+  );
   return (
     <div className="bcds-Navbar" {...props}>
       <div className="bcds-Navbar--Container" role="navigation">
-        {children}
+        {childrenArray.map((child, index) => (
+          <React.Fragment key={index}>
+            {child}
+            {index < childrenArray.length - 1 && <Separator />}
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
