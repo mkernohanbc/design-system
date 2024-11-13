bd7e90ef7dbfabfbcc78be647e98b76dba2d1540
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-11-13 13:38:25 -0800

    Navbar collapses into hamburger menu at smaller screen sizes

--- packages/react-components/src/components/Navbar/Navbar.tsx
+++ packages/react-components/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,10 @@
 import "./Navbar.css";
 import React from "react";
+import useWindowDimensions from "@/hooks/useWindowDimensions";
+import Menu from "../Menu";
+import SvgMenuIcon from "../Icons/SvgMenuIcon";
+import Button from "../Button";
+import MenuItem from "../MenuItem";
 
 export interface NavbarProps extends React.PropsWithChildren {
   children?: React.ReactNode;
@@ -11,15 +16,38 @@ export default function Navbar({ children, ...props }: NavbarProps) {
   const Separator = () => (
     <span className="bcds-Navbar--Separator" role="separator" />
   );
+  /* Collapse navbar at smaller screen sizes */
+  const { isSmall } = useWindowDimensions();
   return (
     <div className="bcds-Navbar" {...props}>
       <nav className="bcds-Navbar--Container">
-        {childrenArray.map((child, index) => (
-          <React.Fragment key={index}>
-            {child}
-            {index < childrenArray.length - 1 && <Separator />}
-          </React.Fragment>
-        ))}
+        {isSmall ? (
+          <>
+            {childrenArray.map((child, index) => (
+              <React.Fragment key={index}>
+                {child}
+                {index < childrenArray.length - 1 && <Separator />}
+              </React.Fragment>
+            ))}
+          </>
+        ) : (
+          <>
+            <Menu
+              triggerElement={
+                <Button size="small" variant="secondary">
+                  <SvgMenuIcon />
+                  Menu
+                </Button>
+              }
+            >
+              {childrenArray.map((child, index) => (
+                <React.Fragment key={index}>
+                  <MenuItem>{child}</MenuItem>
+                </React.Fragment>
+              ))}
+            </Menu>
+          </>
+        )}
       </nav>
     </div>
   );
