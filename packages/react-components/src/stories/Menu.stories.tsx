9024fd55b825025d280a7ecd6f1758fdeca15c03
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-11-25 11:33:22 -0800

    fleshing out menu docs

--- packages/react-components/src/stories/Menu.stories.tsx
+++ packages/react-components/src/stories/Menu.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button, Menu, MenuItem } from "../components";
+import { Button, Link, Menu, MenuItem, SvgMenuIcon } from "../components";
 import { MenuProps } from "../components/Menu";
 
 const meta = {
@@ -8,9 +8,14 @@ const meta = {
   component: Menu,
   parameters: { layout: "centered" },
   argTypes: {
+    children: {
+      control: { type: "object" },
+      description: "Expects an array of `MenuItem` components",
+    },
     triggerElement: {
       control: { type: "object" },
-      description: "",
+      description:
+        "The component (commonly a `Button`) that activates the menu",
     },
   },
 } satisfies Meta<typeof Menu>;
@@ -19,10 +24,17 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const MenuTemplate: Story = {
-  args: { triggerElement: [<Button>Open the modal</Button>] },
-  render: ({ ...args }: MenuProps<object>) => (
-    <Menu {...args}>
-      <MenuItem></MenuItem>
-    </Menu>
-  ),
+  args: {
+    triggerElement: [
+      <Button variant="secondary">
+        Open the menu <SvgMenuIcon />
+      </Button>,
+    ],
+    children: [
+      <MenuItem>
+        <Link href="#">Item 1</Link>
+      </MenuItem>,
+    ],
+  },
+  render: ({ ...args }: MenuProps<object>) => <Menu {...args} />,
 };
