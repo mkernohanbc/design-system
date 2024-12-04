246b8ac03835f37ff627cbff411947731b1930bc
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-12-04 15:19:30 -0800

    remove overlayarrow from popover

--- packages/react-components/src/components/Popover/Popover.tsx
+++ packages/react-components/src/components/Popover/Popover.tsx
@@ -1,18 +1,13 @@
 import {
-  OverlayArrow,
   Popover as ReactAriaPopover,
   PopoverProps,
 } from "react-aria-components";
 
 import "./Popover.css";
-import SvgTooltipArrowUp from "../Icons/SvgTooltipArrowUp";
 
 export default function Popover(props: PopoverProps) {
   return (
     <ReactAriaPopover className="bcds-react-aria-Popover" {...props}>
-      <OverlayArrow className="bcds-react-aria-OverlayArrow">
-        <SvgTooltipArrowUp />
-      </OverlayArrow>
       <>{props.children}</>
     </ReactAriaPopover>
   );
